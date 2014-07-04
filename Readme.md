7df6523c75a840985d916be7e248a9be2fa6bbe3
Taylor Smith <tsmit182@gmail.com>
2014-07-04 18:31:12 -0400

    added some content; realized project outside of current skill level of dev. game put on pause.

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-This is going to be a game! 
+Intended to be a Tamogotchi-style game in which different monsters are selected and cared for as they slowly drain your health, happiness, or sanity.
+
+This project has been 'put on pause' (hey, game pun!) for the time being.
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('The Iron Yard Rocks');
+var Player = function (options) {
+	var options = options || {};
+	this.sanity = options.sanity
+}
+
+var Monster = function (options) {
+	var options = options || {};
+	this.health = options.health;
+	this.happiness = options.happiness;
+}
